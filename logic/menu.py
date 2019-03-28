15cc9b2974050347e917ff8d47ec56da8cf60a2b
varo15 <37835826+varo15@users.noreply.github.com>
2019-03-28 21:26:21 +0100

    Crear Ventana Crear Frame Crear Label Crear Entry Crear Boton Crear Radio Boton

--- logic/menu.py
+++ logic/menu.py
@@ -1,16 +1,17 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.ttk import Radiobutton
 
 root = tk.Tk()
-root.config(width=300, height=200)
+root.config(width=300, height=400)
 root.title("Titulo de la ventana")
-#Crear el Frame
+# Crear el Frame
 frame = ttk.Frame(root)
 
 # Crear Label
 label = ttk.Label(root, text="Etiqueta")
-#Posicionar la etiqueta
-label.place(x=10, y=10)
+# Posicionar la etiqueta
+label.place(x=50, y=20)
 
 # Crear caja de texto.
 entry = ttk.Entry(root)
@@ -18,10 +19,25 @@ entry = ttk.Entry(root)
 entry.place(x=50, y=50)
 
 # Crear campo Text
-T = tk.Text(root, height=2, width=30)
-entry.place(x=70, y=70)
-T.insert(tk.END, "Just a text Widget\nin two lines\n")
 
 
-frame.mainloop() #Fin Frame
-root.mainloop() #Fin root
+# Crear un Boton
+button = ttk.Button(root, text = "Boton")
+button.place(x=50, y=100)
+
+# Crear Radio Button
+var = tk.IntVar()
+R1 = Radiobutton(root, text="Option 1", variable=var, value=1)
+R1.pack(anchor =tk.W)
+R1.place(x=50, y=150)
+
+R2 = Radiobutton(root, text="Option 2", variable=var, value=2)
+R2.pack(anchor =tk.W)
+R2.place(x=50, y=170)
+
+R3 = Radiobutton(root, text="Option 3", variable=var, value=3)
+R3.pack(anchor =tk.W)
+R3.place(x=50, y=190)
+
+frame.mainloop()  # Fin Frame
+root.mainloop()  # Fin root
