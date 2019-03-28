7a6c80c6c099a745d2b1d740e2a03b83807e12e4
varo15 <37835826+varo15@users.noreply.github.com>
2019-03-28 21:15:36 +0100

    Crear Ventana Crear Frame Crear Label Crear Entry

--- logic/menu.py
+++ logic/menu.py
@@ -1,8 +1,27 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
 
-root = Tk()
+root = tk.Tk()
+root.config(width=300, height=200)
+root.title("Titulo de la ventana")
+#Crear el Frame
+frame = ttk.Frame(root)
 
-menubar = Menu(root)
-root.config(menu=menubar)  # Lo asignamos a la base
+# Crear Label
+label = ttk.Label(root, text="Etiqueta")
+#Posicionar la etiqueta
+label.place(x=10, y=10)
 
-root.mainloop()
+# Crear caja de texto.
+entry = ttk.Entry(root)
+# Posicionarla en la ventana.
+entry.place(x=50, y=50)
+
+# Crear campo Text
+T = tk.Text(root, height=2, width=30)
+entry.place(x=70, y=70)
+T.insert(tk.END, "Just a text Widget\nin two lines\n")
+
+
+frame.mainloop() #Fin Frame
+root.mainloop() #Fin root
